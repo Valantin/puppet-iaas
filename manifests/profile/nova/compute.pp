61f4ea0bdaee0d9995059dc3e726d7c548006115
Quentin MACHU <hello@quentin-machu.fr>
2015-03-18 18:36:10 +0100

    Added some required sysctl parameters for Nova Compute + Neutron

--- manifests/profile/nova/compute.pp
+++ manifests/profile/nova/compute.pp
@@ -10,7 +10,11 @@ class iaas::profile::nova::compute (
   include iaas::profile::nova::common
 
   package { 'sysfsutils': }
-  
+
+  sysctl { 'net.ipv4.ip_forward': value => '1' }
+  sysctl { 'net.ipv4.conf.all.rp_filter': value => '0' }
+  sysctl { 'net.ipv4.conf.default.rp_filter': value => '0' }
+
   class { 'ceph::profile::client': } ->
   class { 'ceph::keys': }
 
